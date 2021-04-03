4716a680582aee3aea5cbbe1ee86926aa07e679c
Nickolay Klyuev <nickolay.klyuev@gmail.com>
2021-04-03 17:48:33 +0300

    add record and play back methods

--- Assets/Scripts/ReplayScript.cs
+++ Assets/Scripts/ReplayScript.cs
@@ -4,16 +4,38 @@ using UnityEngine;
 
 public class ReplayScript : MonoBehaviour
 {
+    private const int bufferFrames = 100;
+    private KeyFrame[] keyFrames = new KeyFrame[bufferFrames];
+
+    private Rigidbody rigidBody;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidBody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Record();
+    }
+
+    void PlayBack()
+    {
+        rigidBody.isKinematic = true;
+        int frame = Time.frameCount % bufferFrames;
+        transform.position = keyFrames[frame].position;
+        transform.rotation = keyFrames[frame].rotation;
+    }
+
+    void Record()
+    {
+        rigidBody.isKinematic = false;
+        int frame = Time.frameCount % bufferFrames;
+        float time = Time.time;
+
+        keyFrames[frame] = new KeyFrame(time, transform.position, transform.rotation);
     }
 }
 
